b364525979a9c286083a2a72fce62698889e4bc8
Emona Santiago <emonasantiago1207@gmail.com>
2023-03-23 18:42:45 +0100

    header without styling
    header bar content without styling

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<header>
+  <img
+    id="logo"
+    src="https://avatars.githubusercontent.com/u/62932179?s=280&v=4"
+    alt="fdnd-logo"
+  />
+  <ul>
+    <li>Aanmelden</li>
+    <li>FDND.nl</li>
+  </ul>
+</header>
+
+<style>
+  header {
+    background-color: blue;
+  }
+  img {
+    width: 5em;
+  }
+
+  ul {
+    float: right;
+  }
+
+  li {
+    display: inline-block;
+    padding: 1em;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+  import "/Users/emonasantiago/Documents/choices-choices-the-tech-stack/WLW-LetsJam/src/global.css";
+</script>
+
+<slot />
