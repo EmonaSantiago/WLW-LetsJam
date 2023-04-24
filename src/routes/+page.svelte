4738dad72e9c7596bfd173f257382e550501c7c2
Emona Santiago <emonasantiago1207@gmail.com>
2023-04-24 14:46:24 +0200

    added route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
   import Header from "$lib/components/header.svelte";
   import Hero from "$lib/components/hero.svelte";
   import Maincontent from "$lib/components/maincontent.svelte";
+  import Content from "$lib/components/content.svelte";
 </script>
 
 <Header />
 <Hero />
 <Maincontent />
+<Content />
